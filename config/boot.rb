a504f1f0d00f80849afd4239de8e53dd9143f922
guillermo hernandez <guilhercasanova@hotmail.com>
2015-05-23 21:54:08 -0700

    project

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,11 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+module Rails
+  class Server
+    def default_options
+      super.merge!(:Port => 3000)
+    end
+  end
+end
